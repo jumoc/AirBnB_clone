7f11e93b5216eff43fd6e6b0d64ad0b18a538c9e
Jumoc <juanpcaicedo2@gmail.com>
2021-02-16 17:55:00 -0500

    Add chenges to the file storage and console

--- console.py
+++ console.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """module"""
 import cmd
+import shlex
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
+    classes = {"BaseModel": BaseModel, "User": User}
     """class that inherits from cmd.Cmd
 
     .cmdloop()          ---> keeps the cmd opened
@@ -30,6 +35,110 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+    def search_instance(self, args):
+        n_dict = storage.all().copy()
+        for key in n_dict:
+            if (n_dict[key]["id"] == args[1]) and (n_dict[key]["__class__"] == args[0]):
+                instance = HBNBCommand.classes[n_dict[key]["__class__"]](**n_dict[key])
+                return instance
+        return None
+
+    def do_create(self, line):
+        # check if user didn´t type a class name
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        # check if class name exist
+        if args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        instance = HBNBCommand.classes[args[0]]()
+        print(instance.id)
+        instance.save()
+
+    def do_show(self, line):
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if not args[0] in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance = self.search_instance(args)
+        if instance is not None:
+            print(instance)
+        else:
+            print("** no instance found **")
+
+    # DRY = dont repeat yourself
+
+    def do_destroy(self, line):
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if not args[0] in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        n_dict = storage.all()
+        for key in n_dict:
+            if (n_dict[key]["id"] == args[1]) and (n_dict[key]["__class__"] == args[0]):
+                n_dict.pop(key)
+                storage.__objects = n_dict
+                storage.save()
+                return
+        print("** no instance found **")
+
+    def do_all(self, line):
+        args = shlex.split(line)
+        instances = []
+        n_dict = storage.all().copy()
+
+        if len(args) > 0 and not args[0] in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+            return
+
+        for i in n_dict:
+            instance = None
+            if len(args) == 0:
+                instance = HBNBCommand.classes[n_dict[i]["__class__"]](**n_dict[i])
+            elif args[0] == n_dict[i]["__class__"]:
+                instance = HBNBCommand.classes[n_dict[i]["__class__"]](**n_dict[i])
+            if instance is not None:
+                instances.append(instance.__str__())
+        print(instances)
+
+    def do_update(self, line):
+        args = shlex.split(line)
+        # update <class name> <id> <attribute name> "<attribute value>"
+        if len(args) == 0:
+            print("** class name missing **")
+        elif not args[0] in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            instance = self.search_instance(args)
+            if instance is None:
+                print("** no instance found **")
+            else:
+                #¡Castear el tipo de objeto a su type corresponiente!
+                instance.__dict__[args[2]] = args[3]
+                instance.save()
 
 """call the cmdloop() on the class HBNB (casted as a
 instance with "()"), it can also be done as follows
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 import json
 
 
+
 class FileStorage():
     __file_path = "file.json"
     __objects = {}
@@ -14,11 +15,17 @@ class FileStorage():
             obj.__class__.__name__,
             obj.id
         )
-        FileStorage.__objects[n_obj] = obj.to_dict()
+        FileStorage.__objects[n_obj] = obj
 
     def save(self):
+        new_dict = {}
+        for element in FileStorage.__objects:
+            obj = FileStorage.__objects[element]
+            obj_dict = obj.to_dict()
+            new_dict[element] = obj_dict
+
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            f.write(json.dumps(FileStorage.__objects))
+            f.write(json.dumps(new_dict))
 
     def reload(self):
         try:
