e1995aa7b0eee5483de8ed39dc5acbafb3d5ceca
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-17 18:29:24 -0500

    test EOF added

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,12 +10,21 @@ from console import HBNBCommand
 class testCommand(unittest.TestCase):
     """Command test"""
 
-    def test_quit(self):
+    def test_help_quit(self):
         """test for quit command"""
+        message = "Quit command to exit the program\n\n"
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("quit")
-        value = f.getvalue()
-        self.assertEqual(value, True)
+            HBNBCommand().onecmd("help quit")
+        stdo = f.getvalue()
+        self.assertEqual(message, stdo)
+
+    def test_help_EOF(self):
+        """test for EOF [CTRL + D] command"""
+        message = "EOF (CTRL + D) command to exit the program\n\n"
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+        stdo = f.getvalue()
+        self.assertEqual(message, stdo)
 
 
 if __name__ == "__main__":
