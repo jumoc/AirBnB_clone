a29537b1692fc2879156e32deaec2664004c284b
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-17 14:57:33 -0500

    import modules and methods

--- console.py
+++ console.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 """module"""
 import cmd
+import shlex
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 
 
 class HBNBCommand(cmd.Cmd):
     """class that inherits from cmd.Cmd"""
+    classes = {
+        "BaseModel": BaseModel, "User": User,
+        "State": State, "City": City, "Amenity": Amenity,
+        "Review": Review, "Place": Place
+    }
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
