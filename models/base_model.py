c1b52fde25cc24efaf0dd73c6df417f77413ea16
Jumoc <juanpcaicedo2@gmail.com>
2021-02-17 01:08:16 -0500

    Fix code

--- models/base_model.py
+++ models/base_model.py
@@ -9,18 +9,17 @@ class BaseModel():
     """BaseModel parent class"""
 
     def __init__(self, *args, **kwargs):
-
-        if len(kwargs) == 0:
+        if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
         else:
-            for item in kwargs:
-                if item == "updated_at" or item == "created_at":
-                    kwargs[item] = datetime.fromisoformat(kwargs.get(item))
-                if item != "__class__":
-                    setattr(self, item, kwargs.get(item))
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    kwargs[key] = datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         # [<class name>] (<self.id>) <self.__dict__>
@@ -37,7 +36,7 @@ class BaseModel():
     def to_dict(self):
         """Returns a dictionary representation of an object"""
         n_dict = self.__dict__.copy()
-        n_dict["__class__"] = self.__class__.__name__
+        n_dict["__class__"] = type(self).__name__
         n_dict["created_at"] = n_dict.get("created_at").isoformat()
         n_dict["updated_at"] = n_dict.get("updated_at").isoformat()
         return n_dict
