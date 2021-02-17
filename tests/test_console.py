5959c39ffa8ecd8df608e52fa516b366411a7f4a
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-17 18:45:03 -0500

    Correct output EOF and quit

--- tests/test_console.py
+++ tests/test_console.py
@@ -15,7 +15,14 @@ class testCommand(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("quit")
         stdo = f.getvalue()
-        self.assertEqual(stdo, True)
+        self.assertEqual(stdo, "")
+
+    def test_EOF(self):
+        """test for EOF [CTRL + D] command"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("EOF")
+        stdo = f.getvalue()
+        self.assertEqual(stdo, "\n")
 
 
 if __name__ == "__main__":
