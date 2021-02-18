d1fe0afb14025cdb099472cc8f48cf7a89427c2d
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-17 19:04:26 -0500

    Test for commands and its help

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,12 +17,28 @@ class testCommand(unittest.TestCase):
         stdo = f.getvalue()
         self.assertEqual(stdo, "")
 
+    def test_help_quit(self):
+        """test for help quit command"""
+        message = "Quit command to exit the program\n\n"
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
+        stdo = f.getvalue()
+        self.assertEqual(stdo, message)
+
     def test_EOF(self):
-        """test for EOF [CTRL + D] command"""
+        """test for EOF (CTRL + D) command"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("EOF")
         stdo = f.getvalue()
         self.assertEqual(stdo, "\n")
+
+    def test_help_EOF(self):
+        """test for help quit command"""
+        message = "EOF (CTRL + D) command to exit the program\n\n"
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+        stdo = f.getvalue()
+        self.assertEqual(stdo, message)
 
 
 if __name__ == "__main__":
