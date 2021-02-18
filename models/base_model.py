ceb4d1fd419ded9ece26351cb2f4b0edf28bf5b7
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-17 20:26:00 -0500

    upload file

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ class BaseModel():
     """BaseModel parent class"""
 
     def __init__(self, *args, **kwargs):
+        """Constructor for BaseModel"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
